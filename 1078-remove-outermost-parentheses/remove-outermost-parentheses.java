3602f5a7aba4a097de1f34c93434c0e30b92dde9
SUNYALA NAINA DEVI <nainadevi961@gmail.com>
2026-07-20 11:59:55 +0530

    Time: 2 ms (99.70%) | Memory: 43.3 MB (83.96%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder res=new StringBuilder();
+        int level=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='('){
+                if(level>0) res.append(ch);
+                level++;
+            }else if(ch==')'){
+                level--;
+                if(level>0)res.append(ch);
+            }
+        }
+        return res.toString();
+    }
+}
